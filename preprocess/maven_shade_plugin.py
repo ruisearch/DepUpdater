fa8f182aa5f9be000b9ce085983b0751d8d81e87
ruisearch <731201083@qq.com>
2024-03-26 23:13:22 +0800

    xml.etree is hard to use. planning to use BS4 to process pom.xml

--- preprocess/maven_shade_plugin.py
+++ preprocess/maven_shade_plugin.py
@@ -7,16 +7,24 @@ def insert(pom_file_path:str):
     tree = ET.parse(pom_file_path)
     root = tree.getroot()
     
-    # Define the namespace and register it with a prefix
-    namespace = "http://maven.apache.org/POM/4.0.0"
-    prefix = "ns"
-    namespaces = {"ns": namespace}
-    
+    ## Define the namespace mappings
+    ## map URI to prefix
+    ns = {
+        "xmlns":"http://maven.apache.org/POM/4.0.0"
+    }
+    # # Define the namespace URI
+    # namespace_uri = "http://example.com/namespace"
+
+    # # Register the namespace with an empty prefix
+    # ET.register_namespace("", namespace_uri)
 
     # Check if Maven Shade Plugin is already declared
     is_maven_shade_plugin_declared = False
     for plugin in root.findall(".//build/plugins/plugin"):
+    # for plugin in root.findall(".//xmlns:build/xmlns:plugins/xmlns:plugin", ns):
+        # groupId = plugin.find("xmlns:groupId", ns)
         groupId = plugin.find("groupId")
+        # artifactId = plugin.find("xmlns:artifactId", ns)
         artifactId = plugin.find("artifactId")
         if groupId is not None and groupId.text == "org.apache.maven.plugins" and artifactId is not None and artifactId.text == "maven-shade-plugin":
             is_maven_shade_plugin_declared = True
@@ -25,14 +33,11 @@ def insert(pom_file_path:str):
     # Only add Maven Shade Plugin if it's not already declared
     if not is_maven_shade_plugin_declared:
         # Find the <build> tag
-        # build_tag = root.find("build")
-        build_tag = root.find("ns:build", namespaces)
-
-        # debug
-        if build_tag is None:
-            print("no build")
+        # build_tag = root.find("xmlns:build", ns)
+        build_tag = root.find("build")
         
         # Find or create the <plugins> tag within <build>
+        # plugins_tag = build_tag.find("xmlns:plugins", ns)
         plugins_tag = build_tag.find("plugins")
         if plugins_tag is None:
             plugins_tag = ET.SubElement(build_tag, "plugins")
