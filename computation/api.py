84f71115caee613798b8b8c3dabf746d30174fab
ruisearch <731201083@qq.com>
2024-08-23 21:44:49 +0800

    test entry points and reachable api

--- computation/api.py
+++ computation/api.py
@@ -8,7 +8,7 @@ from constants import SOOTCG_PATH,SOOT_TYPE_DG_PATH,REACHABLE_API_DIR
 from preprocess.Restore import Restore
 
 class Api:
-    def __init__(self, groupId:str, artifactId:str, version:str, ):
+    def __init__(self, groupId:str, artifactId:str, version:str):
         self.groupId = groupId
         self.artifactId = artifactId
         self.version = version
@@ -61,8 +61,10 @@ class Api:
             # caller -> callee in one line, extract caller and callee
             if '->' in line:
                 caller, callee = line.split(' -> ')
-                methods.add(caller.strip())
-                methods.add(callee.strip())
+                if self.filter_method(caller):
+                    methods.add(caller.strip())
+                if self.filter_method(callee):
+                    methods.add(callee.strip())
         return methods
     
     def extract_types_from_dg(self, dg:str):
@@ -72,9 +74,34 @@ class Api:
             # caller -> callee in one line, extract caller and callee
             if '->' in line:
                 caller, callee = line.split(' -> ')
-                types.add(caller.strip())
-                types.add(callee.strip())
+                if self.filter_type(caller):
+                    types.add(caller.strip())
+                if self.filter_type(callee):
+                    types.add(callee.strip())
         return types
+    
+    def filter_method(self, method:str):
+        """filter built-in methods
+        if methods are built-in methods, return False
+        """
+        # extract class name from method
+        # class name is between the first ' ' and '::'
+        class_name = method.split(' ')[1].split('::')[0]
+        return self.filter_type(class_name)
+            
+    def filter_type(self, type:str):
+        """filter built-in types
+        if types are built-in types, return False
+        """
+        return type.startswith('java.') is False \
+            and type.startswith('javax.') is False \
+                and type.startswith('sun.') is False \
+                    and type.startswith('com.sun.') is False \
+                        and type.startswith('sunw.') is False \
+                            and type.startswith('com.ibm.') is False \
+                                and type.startswith('com.apple.') is False \
+                                    and type.startswith('apple.awt.') is False \
+                                        and type.startswith('jdk.internal.') is False
 
     @staticmethod
     def parse_call_relations(call_relations_str:str):
@@ -87,8 +114,6 @@ class Api:
             key: caller, value: a set of the corresponding callees\n
         """
         call_relations = {}
-        callers = set()
-        callees = set()
         
         # Split the string by lines
         lines = call_relations_str.strip().split('\n')
@@ -96,8 +121,6 @@ class Api:
         # Process each line
         for line in lines:
             caller, callee = line.strip().split(' -> ')
-            callers.add(caller)
-            callees.add(callee)
             if caller in call_relations:
                 call_relations[caller].add(callee)
             else:
@@ -141,8 +164,27 @@ class Api:
         return reachable
 
 if __name__ == '__main__':
-    api = Api('joda-time', 'joda-time', '2.12.7')
-    print("get cg")
-    api.get_cg()
-    print("get type dg")
-    api.get_type_dg()
+    # # test cg and dg
+    # api = Api('joda-time', 'joda-time', '2.12.7')
+    # print("get cg")
+    # api.get_cg()
+    # print("get type dg")
+    # api.get_type_dg()
+    
+    # # test find_reachable_calls
+    # entry_points = set(['api_1', 'api_3'])
+    # call_relations = {
+    #     'api_1' : set(['api_4']),
+    #     'api_2' : set(['api_3']),
+    #     'api_3' : set(['api_5'])
+    # }
+    # print(Api.find_reachable_calls(entry_points, call_relations))
+    
+    # test find_matching_relations
+    target_callers = set(['api_6'])
+    call_relations = {
+        'api_1' : set(['api_4', 'api_6']),
+        'api_2' : set(['api_3']),
+        'api_3' : set(['api_5'])
+    }
+    print(Api.find_matching_relations(call_relations, target_callers))
