f614db4959f8f1f83afc602c1a8fd2d085a63b49
ruisearch <731201083@qq.com>
2024-10-04 19:08:19 +0800

    feat: compute tech lag for evaluation

--- MainProcess.py
+++ MainProcess.py
@@ -1,12 +1,14 @@
 """Main process of the tool"""
 import os
 import sys
+import csv
 
 
 from preprocess import exec_maven_command
 from preprocess.Restore import Restore
 from traverse.traverse import Traverse
-from constants import set_log_path, set_soot_empty_cases_csv, RET_DIR
+from constants import set_log_path, set_soot_empty_cases_csv, RET_DIR, DATA_DIR
+from evaluation.tech_lag import TechLag
 
 
 def expand_resolve_abspath(path):
@@ -68,31 +70,12 @@ print("\n****** preprocess done! ******\n")
 tra = Traverse(json_path, path_to_folder, relative_path_to_module)
 tra.traverse()
 
-# # match:
-# # input ：the ./data/Jar folder containing all jars
-# # output : the mapping from dep jars to APIs which are called directly or transitively by client
-# print("\n****** match ... ******\n")
-# # match.all(relative_path_to_module)
-# match.all()
-# print("\n****** match done ! ******\n")
+# compute the technical lag of the module
+print("\n****** compute technical lag ... ******\n")
 
-# # create folder to contain false_cases; the false negative cases and false positive cases will be stored in false_cases/
-# # project_error_folder(like _home_ray_Work_Tool_Data_fudan_paper_client_584_java-design-patterns/ in false_cases/) represents a project rather than a module
-# # False_case_path is the folder to store false cases/
-# False_case_path = os.path.join(os.getcwd(), 'false_cases/')
-# project_name = path_to_folder.replace('/','_')
-# project_error_folder = os.path.join(False_case_path, project_name)
-# if os.path.isdir(project_error_folder) is False:
-#     # if project_error_folder doesn't exist, create one
-#     # don't delete the existing one as the it may contain other modules
-#     # note: folder representing module of the project is the subfolder of project_error_folder
-#     os.makedirs(project_error_folder)
-    
-
-# ## calculate:
-# ## input : the ./data/Jar folder containing version.json
-# ## output : the newest compatible version of each dep in version.json and finial lag result(after test)
-# print("\n****** calculate: ******\n")
-# cal.select_best_version(path_to_folder, project_error_folder)
-# cal.calculate_lag()
-# print("\n****** calculate done ! ******\n")
+lag = TechLag(json_path)
+# record the original lag, current lag and reduced lag in a csv file
+lag_csv_path = os.path.join(DATA_DIR, 'lag.csv')
+with open(lag_csv_path, 'a') as f:
+    writer = csv.writer(f)
+    writer.writerow([repo_name, relative_path_to_module, lag.original_lag, lag.current_lag, lag.reduced_lag])
---
+++ evaluation/tech_lag.py
@@ -0,0 +1,45 @@
+"""class to compute technical lag"""
+import json
+
+class TechLag:
+    def __init__(self, json_path):
+        """Args:
+        json_path: str, path to the json file containing the versions of the deps in the module
+        """
+        self.json_path = json_path
+        self.deps = self.extract_deps()
+        self.original_lag = 0
+        self.current_lag = 0
+        self.compute_original_lag()
+        self.compute_current_lag()
+        self.reduced_lag = self.original_lag - self.current_lag
+
+    def extract_deps(self):
+        """extract dep from json"""
+        with open(self.json_path, 'r') as f:
+            deps = json.load(f)
+        ret_deps = []
+        for dep in deps:
+            if dep['Best_Version'] and dep['Dependents']:
+                # consider the deps that has the best version and has dependents
+                ret_deps.append(dep)
+        return ret_deps
+
+    def compute_original_lag(self):
+        """compute the original lag of the module"""
+        for dep in self.deps:
+            self.original_lag += len(dep['Dependents']) - 1
+
+    def compute_current_lag(self):
+        """compute the current lag of the module"""
+        for dep in self.deps:
+            all_versions = dep['Versions']
+            best_version = dep['Best_Version']
+            idx = self.find_idx(all_versions, best_version)
+            self.current_lag += idx
+
+    def find_idx(self, all_versions:list, version:str):
+        """find the index of the version in all versions"""
+        for idx, v in enumerate(all_versions):
+            if v['version'] == version:
+                return idx
