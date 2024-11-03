63cae4c147163d2f7ae673f6a77a367be0cd89f7
ruisearch <731201083@qq.com>
2024-11-03 18:58:21 +0800

    feat: count the numher of dependencies before and after update

---
+++ evaluation/dep_count.py
@@ -0,0 +1,10 @@
+import json
+
+def count_deps(json_path):
+    with open(json_path, 'r') as f:
+        deps = json.load(f)
+    count = 0
+    for dep in deps:
+        if dep['Best_Version'] and dep['Dependents']:
+            count += 1
+    return count
