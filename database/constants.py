92666199c401fff05781840b3682ab289e7907f0
MarkLee131 <kaixuan.li@ntu.edu.sg>
2025-01-09 15:17:23 +0800

    fix: change the locations of the 3 db files in corresponding cofig files

--- database/constants.py
+++ database/constants.py
@@ -3,9 +3,10 @@ import os
 
 CURRENT_FILE_PATH = os.path.abspath(__file__)
 CURRENT_DIR_PATH = os.path.dirname(CURRENT_FILE_PATH)
+# ROOT_DIR is the root directory of our tool
 ROOT_DIR = os.path.join(CURRENT_DIR_PATH, os.pardir)
 # path to sqlite file(it locates in the root dir of this project)
-SQLITE_PATH = os.path.join(ROOT_DIR, '..','reusable_data.sqlite')
+SQLITE_PATH = os.path.join(ROOT_DIR, 'reusable_data.sqlite')
 # port to connect to the mongodb
 MONGO_PORT = 27017
 # host name of the mongodb
